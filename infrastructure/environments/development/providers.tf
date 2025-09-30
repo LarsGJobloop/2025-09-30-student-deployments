c7ac5157beee33fe1da4ce2e656753272011fd11
zabronax <zabronax@gmail.com>
2025-09-30 15:49:31 +0200

    initialize development iac configuration

---
+++ infrastructure/environments/development/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.53.1"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  description = "Hetzner API token"
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
