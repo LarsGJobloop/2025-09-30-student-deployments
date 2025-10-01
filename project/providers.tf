5a0aa3b8396e0967c8426ace1dac3f3595ec521e
zabronax <zabronax@gmail.com>
2025-10-01 14:12:00 +0200

    setup project iac providers

---
+++ project/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "6.6.0"
+    }
+  }
+}
+
+variable "github_token" {
+  description = "GitHub API token for managing the repository"
+  sensitive = true
+}
+
+provider "github" {
+  token = var.github_token
+}
