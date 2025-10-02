c149d17d0e504c9a526ca952de1e6c6b3878c415
zabronax <zabronax@gmail.com>
2025-10-02 19:09:09 +0200

    update reconciliation script to force clean and overwrite local history

--- infrastructure/modules/hetzner-compose-server/reconciliation-script.sh
+++ infrastructure/modules/hetzner-compose-server/reconciliation-script.sh
@@ -8,8 +8,10 @@ if [ ! -d "$REPO_DIR/.git" ]; then
     echo "[INFO] Initial clone of repo..."
     git clone --branch "${branch}" "${git_remote}" "$REPO_DIR"
 else
-    echo "[INFO] Updating existing repo..."
-    git -C "$REPO_DIR" pull
+    echo "[INFO] Syncing with remote repo..."
+    git -C "$REPO_DIR" fetch origin "${branch}"
+    git -C "$REPO_DIR" reset --hard "origin/${branch}"
+    git -C "$REPO_DIR" clean -fd
 fi
 
 docker compose --file "$COMPOSE_FILE" pull
